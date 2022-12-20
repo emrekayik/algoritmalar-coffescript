ca6afabae9f87149362953c935a002f8e6b48a1a
emrekayik <emrekayik1905@gmail.com>
2022-12-21 01:53:29 +0300

    algoritmalar adında başka bir klasörde toplama

--- dist/index.js
+++ dist/index.js
@@ -3,17 +3,17 @@
   // Tek-Çift sayı
   var asal, tambolen, tek_cift;
 
-  tek_cift = require('./tekcift');
+  tek_cift = require('./algoritmalar/tekcift');
 
   tek_cift.tek_cift(2);
 
   // Tam bölenler
-  tambolen = require('./tambolen');
+  tambolen = require('./algoritmalar/tambolen');
 
   tambolen.tambolen(10);
 
   // Asal sayı
-  asal = require('./asal');
+  asal = require('./algoritmalar/asal');
 
   asal.asal(10);
 
--- src/index.coffee
+++ src/index.coffee
@@ -1,11 +1,11 @@
 # Tek-Çift sayı
-tek_cift = require './tekcift'
+tek_cift = require './algoritmalar/tekcift'
 tek_cift.tek_cift(2)
 
 # Tam bölenler
-tambolen = require './tambolen'
+tambolen = require './algoritmalar/tambolen'
 tambolen.tambolen(10)
 
 # Asal sayı
-asal = require './asal'
+asal = require './algoritmalar/asal'
 asal.asal(10)
